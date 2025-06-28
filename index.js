6819547c4c71ec290b762276c832d1e1e3fb30ee
l22nj <211811295+l22nj@users.noreply.github.com>
2025-06-28 13:35:19 +0300

    made the ui :))

--- index.js
+++ index.js
@@ -2,85 +2,77 @@ function main() {
 
     let computerScore = 0;
     let humanScore = 0;
-    let prevWinner = "";
+    let playCount = 0;
+
+    let buttonRock = document.querySelector("#rock");
+    let buttonPaper = document.querySelector("#paper");
+    let buttonScissors = document.querySelector("#scissors");
+    let scoreCount = document.querySelector("#score");
+    let winnerText = document.querySelector("#winner");
+
+    buttonRock.addEventListener("click", playAsRock);
+    buttonPaper.addEventListener("click", playAsPaper);
+    buttonScissors.addEventListener("click", playAsScissors);
 
     function getComputerChoice() {
         return Math.floor(Math.random() * 3) % 3;
     }
 
-    function getHumanChoice() {
-        let promptText = prevWinner + `human: ${humanScore}, computer: ${computerScore}\nRock, paper or scissors?`
-        let choice = prompt(promptText).toLowerCase();
-        switch (choice) {
-            case "rock": {
-                return 0;
-            }
-            case "paper": {
-                return 1;
-            }
-            case "scissors": {
-                return 2;
-            }
-        }
-    }
-
     function determineWinner(computerChoice, humanChoice) {
+        let spanComputerText = document.querySelector("#computerChoice")
         switch (computerChoice) {
             case 0: {
-                prevWinner = "computer chose rock\n";
+                spanComputerText.innerText = "rock";
                 break;
             }
             case 1: {
-                prevWinner = "computer chose paper\n";
+                spanComputerText.innerText = "paper";
                 break;
             }
             case 2: {
-                prevWinner = "computer chose scissors\n";
+                spanComputerText.innerText = "scissors";
                 break;
             }
         }
         if (computerChoice === humanChoice) {
             humanScore += 1;
             computerScore += 1;
-            prevWinner += "Draw!\n"
         }
         else if (Math.abs(computerChoice - humanChoice) < 2) {
             if (computerChoice < humanChoice) {
                 humanScore += 1
-                prevWinner += "You won!\n"
             }
             else {
                 computerScore += 1;
-                prevWinner += "Computer won :/\n"
             }
         }
         else {
             if (computerChoice < humanChoice) {
                 computerScore += 1;
-                prevWinner += "Computer won :/\n"
             }
             else {
                 humanScore += 1
-                prevWinner += "You won!\n"
             }
         }
-
+        console.log(humanScore + "-" + computerScore)
+        scoreCount.innerText = humanScore + "-" + computerScore;
+        if (++playCount === 5) {
+            let winner;
+            if (humanScore > computerScore) winner = "You won:DDDD";
+            else if (computerScore < humanScore) winner = "You lost:///";
+            else winner = "you got a draw :D";
+            winnerText.innerText = winner;
+        }
     }
-
-    function playRound() {
-        let computerChoice = getComputerChoice();
-        let humanChoice = getHumanChoice();
-        determineWinner(computerChoice, humanChoice);
-        console.log(computerChoice);
-        console.log("humanscore: " + humanScore + " computerscore: " + computerScore);
+    function playAsRock() {
+        determineWinner(getComputerChoice(), 0);
     }
-
-    function playGame() {
-        for (let i= 0; i < 5; ++i) playRound();
+    function playAsPaper() {
+        determineWinner(getComputerChoice(), 1);
     }
-    playGame();
-
-    alert(prevWinner + "\n" + " (" + humanScore + '-' + computerScore + ")");
+    function playAsScissors() {
+        determineWinner(getComputerChoice(), 2);
+    }
 }
 
 main();
